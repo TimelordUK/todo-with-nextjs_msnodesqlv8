9d2ec100883ea740069537d22697a0037a6d7542
TimelordUK <sjames8888@gmail.com>
2023-11-23 21:13:43 +0000

    use a Verb based dispatch

--- pages/api/task/TaskVerbs.ts
+++ pages/api/task/TaskVerbs.ts
@@ -4,24 +4,27 @@ import dbConnect from "../../../utils/dbConnect";
 import {NextApiRequest} from "next";
 
 export class TaskVerbs {
-    public async GET(req: NextApiRequest): Promise<any> {
+
+    private async getConnection(): Promise<ConnectionPromises> {
         const sql = dbConnect()
         console.log(`[index] opening connection`)
         const con: Connection = await sql.driver.promises.open(sql.connStr)
-        const promises: ConnectionPromises = con.promises
+        return con.promises
+    }
+
+    public async GET(req: NextApiRequest): Promise<any> {
+        const promises: ConnectionPromises = await this.getConnection()
         const sqlQuery = 'select _id, task, completed from Task'
         console.log(sqlQuery)
         const q = await promises.query(sqlQuery);
         const tasks: Task[] = q.first
         console.log(tasks)
+        await promises.close()
         return tasks
     }
 
     public async POST(req: NextApiRequest): Promise<any> {
-        const sql = dbConnect()
-        console.log(`[index] opening connection`)
-        const con: Connection = await sql.driver.promises.open(sql.connStr)
-        const promises: ConnectionPromises = con.promises
+        const promises: ConnectionPromises = await this.getConnection()
         await promises.beginTransaction()
         const q1 = `insert into Task (task, completed) values ('${req.body.task}', 0)`
         console.log(q1)
@@ -35,6 +38,7 @@ export class TaskVerbs {
         const fetched = await promises.query(q3)
         const added = fetched.first[0] as Task
         await promises.commit()
+        await promises.close()
         return added
     }
 }
