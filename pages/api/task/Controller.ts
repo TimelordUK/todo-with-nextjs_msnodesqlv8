18a63ff59c9b5731ed3ea041c8cd72cd66e04cdd
TimelordUK <sjames8888@gmail.com>
2023-11-24 22:55:03 +0000

    build and start prod build for demo

--- pages/api/task/Controller.ts
+++ pages/api/task/Controller.ts
@@ -1,58 +1,47 @@
 import {NextApiRequest, NextApiResponse} from "next"
 import {TaskVerbs} from "./TaskVerbs"
 const verbs = new TaskVerbs()
+type Action = (_: NextApiRequest) => Promise<any>
+type ToJSON = (data: any) => any
+
 export class Controller {
     public static async dispatch(req: NextApiRequest, res: NextApiResponse) {
         const {method} = req;
         switch (method) {
             case 'GET': {
-                return verbs.GET(req)
-                    .then(tasks => {
-                        res
-                            .status(200)
-                            .json({data: tasks});
-                    }).catch(e => {
-                    res
-                        .status(500)
-                        .json({message: "Internal Server Error " + e.message});
-                })
+                return Controller.execute(req, res, r => verbs.GET(r),
+                        tasks => { return { data: tasks } }, 200)
             }
 
             case 'POST': {
-                return verbs.POST(req).then(added => {
-                    res
-                        .status(201)
-                        .json({data: added, message: "Task added successfully"});
-                }).catch(e => {
-                    res
-                        .status(500)
-                        .json({message: "Internal Server Error " + e.message});
-                })
+                return Controller.execute(req, res, r => verbs.POST(r),
+                    added => { return { data: added, message: "Task Added successfully" } }, 201)
             }
 
             case 'DELETE': {
-                return verbs.DELETE(req).then(added => {
-                    res
-                        .status(200)
-                        .json({data: added, message: "Task Deleted Successfully"});
-                }).catch(e => {
-                    res
-                        .status(500)
-                        .json({message: "Internal Server Error " + e.message});
-                })
+                return Controller.execute(req, res, r => verbs.DELETE(r),
+                    deleted => { return { data: deleted, message: "Task Deleted successfully" } }, 200)
             }
 
             case 'PUT': {
-                return verbs.PUT(req).then(added => {
-                    res
-                        .status(200)
-                        .json({data: added, message: "Task Updated Successfully"});
-                }).catch(e => {
-                    res
-                        .status(500)
-                        .json({message: "Internal Server Error " + e.message});
-                })
+                return Controller.execute(req, res, r => verbs.PUT(r),
+                    updated => { return { data: updated, message: "Task Updated successfully" } }, 200)
             }
         }
+    }
+
+    private static async execute(req: NextApiRequest, res: NextApiResponse, action: Action, toJson: ToJSON, status: number) {
+        return action(req)
+            .then(data => {
+                res
+                    .status(status)
+                    .json(toJson(data));
+            }).catch(e => {
+                res
+                    .status(500)
+                    .json({
+                        message: "Internal Server Error " + e.message
+                    });
+            })
     }
 }
