b9644d2165ba5201c33332ed485aa781e39bf38d
TimelordUK <sjames8888@gmail.com>
2025-06-25 20:25:05 +0100

    change db string

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -3,7 +3,7 @@ function dbDriver() {
 	if (!global.sql) {
 		global.sql = {
 			driver: require('msnodesqlv8'),
-			connStr: `Driver={ODBC Driver 18 for SQL Server}; Server=DESKTOP-VIUCH90;UID=linux; PWD=linux; Database=node;Encrypt=no;`
+			connStr: `Driver={ODBC Driver 18 for SQL Server};Server=127.0.0.1,1433;Database=node;UID=node_user;PWD=StrongPassword123!;TrustServerCertificate=yes;;Connect Timeout=10`
 		}
 	}
 	return global.sql
